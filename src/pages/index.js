34da8f64821331997072f7847c061351eaeefa2e
Zac B <zacbennett0@gmail.com>
2020-05-09 14:30:13 -0700

    updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,30 +5,22 @@ import { Helmet } from 'react-helmet'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
 import ArticlePreview from '../components/article-preview'
+import RecentProjects from '../components/RecentProjects.jsx'
+import Education from '../components/Education.jsx'
 
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+    const projects = get(this, 'props.data.allContentfulBlogPost.edges')
     const [author] = get(this, 'props.data.allContentfulPerson.edges')
-
+    const education = get(this, 'props.data.allContentfulEducation.edges')
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
           <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+          <RecentProjects projects={projects} />
+          <Education />
         </div>
       </Layout>
     )
